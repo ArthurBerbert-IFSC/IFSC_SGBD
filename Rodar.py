b2f2039255e200e7e1ddb6673d3378943c6f1351
Arthur Peixoto Berbert Lima <141449025+ArthurBerbert-IFSC@users.noreply.github.com>
2025-08-06 18:48:32 -0300

    Add splash screen to application startup

--- Rodar.py
+++ Rodar.py
@@ -1,12 +1,25 @@
 from gerenciador_postgres.gui.main_window import MainWindow
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QSplashScreen
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import QTimer
 import sys
+
 
 def main():
     app = QApplication(sys.argv)
+
+    pixmap = QPixmap()
+    splash = QSplashScreen(pixmap)
+    splash.show()
+    app.processEvents()
+
     window = MainWindow()
     window.show()
+
+    QTimer.singleShot(1000, lambda: splash.finish(window))
+
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
